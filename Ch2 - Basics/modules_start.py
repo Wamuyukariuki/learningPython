e8e7132a079d0cf5985307411b8d3dae376a12e3
Wamuyukariuki <jaxonkariuki@gmail.com>
2024-05-15 12:18:12 +0300

    maths modules

--- Ch2 - Basics/modules_start.py
+++ Ch2 - Basics/modules_start.py
@@ -3,10 +3,16 @@
 
 
 # TODO: import the math module, which contains features for working with mathematics
-
+import math
 
 # TODO: the math module contains lots of pre-built functions
-
+print(math.sqrt(16))
+x = math.pi
+area_of_circle = x * 10 * 10
+print(area_of_circle)
+print(math.e)
+print(math.cos(0))
+print(math.degrees(36))
 
 # TODO: in addition to functions, some modules contain useful constants 
 
